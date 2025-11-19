568f73eaf9bc9526b9bf0da1bf6c4f7e1f090708
sudeeptarlekar <sudeeptarlekar@gmail.com>
2025-11-19 22:41:29 +0100

    Chipmunk MCP server test

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,47 @@
 use anyhow::Result;
-use reqwest::Client;
-use serde::{Deserialize, Serialize};
-use serde_json::json;
+use rmcp::{transport::io::stdio, ServiceExt};
+use tokio::task;
+use tracing_subscriber::{self, filter::EnvFilter};
 
-#[derive(Debug, Default, Serialize, Deserialize)]
-pub struct Response {
-    pub model: String,
-    pub created_at: String,
-    pub response: String,
-    pub done: bool,
-    pub done_reason: String,
-    pub total_duration: usize,
-    pub load_duration: usize,
-}
+use mcp_server::start;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let file_path: &str = "/Users/sudeep.tarlekar/Downloads/logs/plain_dlt.txt";
-    let ollama_url = "http://localhost:11434/api/generate";
-    let client = Client::new();
-    let lines = std::fs::read_to_string(file_path)?
-        .lines()
-        .map(String::from)
-        .collect::<Vec<String>>();
+    let (ai_window, mut server_comm, _client_comm) = start()?;
 
-    let file_data = lines.get(0..100).map(|logs| logs.join("\n")).unwrap();
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env().add_directive(tracing::Level::DEBUG.into()))
+        .with_writer(std::io::stderr)
+        .with_ansi(false)
+        .init();
 
-    let response: Response = client
-        .post(ollama_url)
-        .json(&json!({
-            "model": "llama3",
-            "prompt": format!("Give me regex for filtering out error lines from this log: {}", file_data),
-            "stream": false
-        }))
-        .send()
-        .await?
-        .json()
-        .await?;
+    tracing::info!("Runtime: Starting MCP Server");
 
-    println!("Response : {:?}", response);
+    let service = ai_window
+        .serve(stdio())
+        .await
+        .inspect_err(|err| tracing::error!("Error while starting the AI Server: {err:?}"))?;
+
+    let server_task = task::spawn(service.waiting());
+    let receiver_task = task::spawn(async move {
+        while let Some(task) = server_comm.task_rx.recv().await {
+            tracing::debug!("Runtime: Received Task with parameters: {task:?}");
+        }
+    });
+
+    let (server_task_result, receiver_task_result) = tokio::join!(server_task, receiver_task);
+    match server_task_result {
+        Ok(Ok(reason)) => tracing::debug!("Shutting down server for reason: {reason:?}"),
+        Ok(Err(err)) => tracing::error!("Runtime: Error while starting the server; {err:?}"),
+        _ => tracing::error!("Runtime: Error while joining threads in runtime"),
+    }
+
+    match receiver_task_result {
+        Ok(_) => tracing::debug!("Runtime: Receiver task ended successfully"),
+        Err(err) => tracing::error!("Runtime: Error in receiver task; {err:?}"),
+    }
+
+    tracing::info!("Runtime: Server started and now waiting for the message to receive");
+
     Ok(())
 }
